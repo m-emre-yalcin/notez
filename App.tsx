5dafbd6fe406059e0ff2541b0a5457ea3f42d993
m.emre yalçın <99.emreyalcin@gmail.com>
2022-01-19 10:10:40 +0300

    ...

---
+++ App.tsx
@@ -0,0 +1,71 @@
+import * as React from 'react';
+import {StatusBar, StyleSheet, Image, Button, View, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {TransitionPresets} from '@react-navigation/stack';
+
+import Home from './src/screens/home';
+import About from './src/screens/about';
+
+const Stack = createNativeStackNavigator();
+
+function LogoTitle() {
+  return (
+    <View style={{width: 50, height: 50, justifyContent: 'center'}}>
+      <Image source="https://www.tailorbrands.com/wp-content/uploads/2020/07/mcdonalds-logo.jpg" />
+      <Text>About</Text>
+    </View>
+  );
+}
+
+function App() {
+  return (
+    <>
+      <StatusBar animated={true} backgroundColor="orangered" />
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={({route, navigation}) => ({
+            // gestureEnabled: true,
+            ...TransitionPresets.ModalPresentationIOS,
+          })}>
+          <Stack.Screen
+            name="Home"
+            component={Home}
+            options={{
+              title: 'Anasayfa',
+              ...style.header,
+            }}
+          />
+          <Stack.Screen
+            name="About"
+            component={About}
+            options={{
+              headerTitle: () => <LogoTitle />,
+              headerRight: () => (
+                <Button
+                  onPress={() => alert('This is a button!')}
+                  title="Info"
+                  color="orangered"
+                />
+              ),
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
+  );
+}
+
+const style = StyleSheet.create({
+  header: {
+    headerStyle: {
+      backgroundColor: 'orangered',
+    },
+    headerTintColor: '#fff',
+    headerTitleStyle: {
+      fontWeight: 'bold',
+    },
+  },
+});
+
+export default App;
---
+++ src/screens/about.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import {View, Text, Button} from 'react-native';
+
+const About = ({route, navigation}): Node => {
+  return (
+    <View style={{padding: 20}}>
+      <Text color="orangered">About</Text>
+      {route.params && <Text fontSize={20}>Hello {route.params.name}</Text>}
+      <Button
+        title="Go to about again..."
+        onPress={() => navigation.push('About')}
+      />
+      <Button
+        title="Go back"
+        color="cyan"
+        onPress={() => navigation.goBack()}
+      />
+      <Button
+        title="Go home"
+        color="blue"
+        onPress={() => navigation.navigate('Home')}
+      />
+      <Button
+        title="Go back to first screen in stack"
+        onPress={() => navigation.popToTop()}
+      />
+    </View>
+  );
+};
+
+export default About;
